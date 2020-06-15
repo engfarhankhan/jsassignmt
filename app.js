51761697261b2684c56f8ce58fa45091f3330466
farhan khan <engfarhankhan505@gmail.com>
2020-06-15 23:18:52 +0500

    javascriptassignmt

---
+++ app.js
@@ -0,0 +1,248 @@
+// === Chapter # 1 and task # 1
+// alert("hello world")
+
+// === Chapter # 1 task # 2
+// alert("welcome to coding world")
+
+// === Chapter # 1 task 3
+// alert("Wellcome to jsland...\nHappy Coding!")
+
+// === Chapter # 1 task # 4
+// alert("Welcome to Js Land...")
+// alert("Happy Coding!")
+
+// ===Chapter # 1 task # 5
+// var nt = 'Hello, Console!';
+//console.log(nt);
+
+// === Chapter # 1 task # 6
+//function showAlert() {
+//  var myText = " text you like!";
+//alert(myText);}
+
+// === Chapter # 1 task # 7
+
+
+// === Chapter # 2 task # 1
+// var userName;
+
+// === Chapter # 2 task # 2
+// userName = "Farhan";
+
+// === Chapter # 2 task # 3
+// var message = "hello world";
+/// alert(message);
+
+// === Chapter # 2 task # 4
+// var name = "Qasim";
+// alert(name);
+// var age = "Qasim is 23 years old";
+// alert(age);
+// var win = ("Certified developer");
+// alert(win);
+
+// === Chapter # 2 task # 5
+// alert("Pizza \nPizz \nPiz \nPi \nP");
+
+// === Chapter # 2 task # 6
+// var email = ("My email address is abc@coders.com");
+// alert(email);
+
+// === Chapter # 2 task # 7
+// var book = "I am learning from the Book \nA smarter way to learn javascript";
+// alert(book);
+
+// Chapter # 2 task # 8
+// var text = " HTML content run through JAVASCRIPT";
+// document.write(text);
+
+// === Chapter # 2 task # 9
+// var ja = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(ja);
+
+// === Chapter # 3 task # 1
+// var age = "i am 25 years old";
+// alert(age);
+
+// === Chapter # 3 task # 2
+//var visit = "You have visited this site 14times";
+//alert(visit)
+
+// === Chapter # 3 task # 3
+//var birthYear = +prompt("Enter your birthyear");
+//var b = "My birth year is " + birthYear + "<br>"+"Data type of my decleared variable is number"
+//document.write(b);
+
+
+// === Chapter # 3 task # 4
+//var visitorName = "John Doe" ;
+//var productTitle = "T-shirt(s)" ;
+//var Quantity = 5 ;
+//var a = visitorName + " " +"odered" + " " + Quantity + " " + productTitle + " " + "on XYZ Clothing store.";
+//document.write(a);
+
+// === Chapter # 4 task # 1
+// var username = "john" , var age = "33" var roll= "76"; 
+
+//Declare 5 legal & 5 illegal variable names. 
+//legal variable
+
+//_username
+//user_name
+//$userName
+// userName
+//user11
+
+//illegal variable
+
+//9username
+//@username
+//user@name
+//#username
+//"username"
+
+// === Chapter # 4 task # 2
+//var a = "a)  A heading stating “Rules for naming JS variables" ;
+//var b = "b)  Variable names can only contain ______, ______, ______ and ______ For example $name, _name or name";
+//var c = "d) Variable names are case _________ ";
+//var d = "e) Variable names should not be JS _________ "
+//document.write(a + "<br>" + b + "<br>" + c + "<br>" + d);
+
+// === Chapter # 5 task # 1
+//var a = 5;
+//var b = 5;
+//var c = a+b;
+//document.write(c);
+
+// === Chapter # 5 task # 2
+//substraction
+// var a = 25;
+// var b = 5;
+// var c = a-b;
+// document.write(c);
+
+//multiplication
+// var a = 5;
+// var b = 5;
+// var c = a*b;
+// document.write(c);
+
+//Division
+// var a = 15;
+// var b = 5;
+// var c = a/b;
+// document.write(c);
+
+//Modulus
+// var a = 76;
+// var b = 5;
+// var c = a%b;
+// document.write(c);
+
+// === Chapter # 5 task # 3
+// var a = 5 ;
+// var b = "Initial value:" ;
+// document.write(b + " " + a) ;
+
+// === Chapter # 5 task # 4
+// var a = 5 ;
+// var b = a++ ;
+// var c = "Value after increment is:" ;
+// document.write(c + " " + a) ;
+
+// === Chapter # 5 task # 5
+// var a = 7 ;
+// var b = 6 ;
+// var c = a + b ;
+// var d = "Value after addition is:" ;
+// document.write(c) ;
+
+// === Chapter # 5 task # 6
+// var a = 13 ;
+// var b = a-- ;
+// var c = "Value after decrement is:" ;
+// document.write(c + " " + b) ;
+
+// === Chapter # 5 task # 7
+// var a =2 ;
+// var b = 2 ;
+// var c = a%b ;
+// document.write( "The remainder is : " + " " + c) ;
+
+// === Chapter # 5 task # 8
+// var ticket = +prompt("Number of ticket")
+// totalcost = 600 ;
+// tick = ticket*totalcost ;
+// document.write(tick) ;
+
+// === Chapter # 9-10 task # 1
+// var city = prompt("Which city you live in");
+
+// if (city !== "Karachi") {
+// alert("Welcome to city of lights");
+// }
+
+
+// === Chapter 9-10 task # 2
+
+//var gender = prompt(' Type Gender');
+//if (gender == 'male') {
+//  alert('Good Morning Sir!');
+//} else if (gender == 'female') {
+//  alert('Good Morning madam');
+//}
+
+// === Chapter 9-10 task # 3
+//var signal = prompt('Type signal light indication');
+//if (signal == 'red') {
+//  alert('Must Stop');
+//} else if (signal == 'yellow') {
+//  alert('Ready to move');
+//} else if (signal == 'green') {
+// alert('Move now');
+//}
+
+// === Chapter 9-10 task # 5
+//var a = 4;
+//if (++a === 5) {
+// alert("given condition for variable a is true");
+}
+//User Input & Conditional Statement | JAVASCRIPT
+//Page 2 of 4
+//var b = 82;
+//if (b++ === 83) {
+///  alert("given condition for variable b is true");
+//}
+//var c = 12;
+//if (c++ === 13) {
+//  alert("condition 1 is true");
+//}
+//if (c === 13) {
+//  alert("condition 2 is true");
+//}
+//if (++c < 14) {
+//  alert("condition 3 is true");
+//}
+//if (c === 14) {
+//  alert("condition 4 is true");
+//}
+//var materialCost = 20000;
+//var laborCost = 2000;
+//var totalCost = materialCost + laborCost;
+//if (totalCost === laborCost + materialCost) {
+//  alert("The cost equals");
+//}
+//if (true) {
+//  alert("True");
+//}
+//if (false) {
+//  alert("False");
+//}
+//User Input & Conditional Statement | JAVASCRIPT
+//Page 3 of 4
+//if ("car" < "cat") {
+//  alert("car is smaller than cat");
+//}
+
+// === Chapter 5 task # 6 
+// var bn = prompt("Enter Marks", "Marksheet");
